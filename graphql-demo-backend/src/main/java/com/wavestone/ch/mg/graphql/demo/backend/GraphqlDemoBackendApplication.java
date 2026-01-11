604e5ca1ae37f3565eb54fb5ce8dd3a7d36a65bb
Gabor Moos <gabor.moos@wavestone.com>
2026-01-11 15:47:06 +0100

    initial commit

---
+++ graphql-demo-backend/src/main/java/com/wavestone/ch/mg/graphql/demo/backend/GraphqlDemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wavestone.ch.mg.graphql.demo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDemoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlDemoBackendApplication.class, args);
+    }
+
+}
---
+++ graphql-demo-backend/src/test/java/com/wavestone/ch/mg/graphql/demo/backend/GraphqlDemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wavestone.ch.mg.graphql.demo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
